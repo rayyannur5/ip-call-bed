86de9d30e80d52c78ec2c608a613fb8385d9edb0
Rayyan Nur Fauzan <rayyannur5@gmail.com>
2024-05-20 15:54:58 +0700

    delete tw mode

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import requests
 calling = Event()
 after_calling = Event()
 
-ssid = open("/home/opi1/ip-call-bed/config/ssid.txt", "r")
-pswd = open("/home/opi1/ip-call-bed/config/pass.txt", "r")
-id_r = open("/home/opi1/ip-call-bed/config/id.txt", "r")
+ssid = open("/home/nursecall/ip-call-bed/config/ssid.txt", "r")
+pswd = open("/home/nursecall/ip-call-bed/config/pass.txt", "r")
+id_r = open("/home/nursecall/ip-call-bed/config/id.txt", "r")
 
 wifi.connect(ssid.read(),pswd.read())
 ssid.close()
@@ -142,7 +142,7 @@ def call(sip):
 def checkTwoWay():
     x = requests.get(f'http://{host}/ip-call-server/bed/get_one.php?id={id}').json()
     execute(f"amixer set Master {x['data'][0]['vol']}%")
-    return x['data'][0]['tw'], int(x['data'][0]['vol']), int(x['data'][0]['mic'])
+    return x['data'][0]['mode'], int(x['data'][0]['vol']), int(x['data'][0]['mic'])
 
 setupGPIO()
 execute(f"gpio write {led_1} 0")
@@ -151,7 +151,7 @@ execute(f"gpio write {led_1} 1")
 time.sleep(10)
 checkWifiConnection()
 checkServer()
-tw, vol, mic = checkTwoWay()
+mode, vol, mic = checkTwoWay()
 client.connect(host, 1883, 60)
 client.loop_start()
 setupLinphone()
@@ -179,106 +179,64 @@ before_calling = millis()
 
 send_activation = millis()
 
-if tw == '1':
-    while True:
-        if '0' in execute(f"gpio read {btn_call}"):
-            if not oncall and not after_calling.is_set():
-                call_lock = True
-                before_call_lock = millis()
-            
-            
-        hasil_call_lock = millis() - before_call_lock
-        if call_lock and ('1' in execute(f"gpio read {btn_call}"))  and hasil_call_lock > 200:
-            execute("linphonecsh generic terminate")
-            client.publish(f"call/{id}", payload="1", qos=1, retain=True)
-            calling.set()
-            print("LOG| btn call clicked")
+while True:
+    if '0' in execute(f"gpio read {btn_call}"):
+        if not oncall and not after_calling.is_set():
+            call_lock = True
             before_call_lock = millis()
-            call_lock = False
+        
+        
+    hasil_call_lock = millis() - before_call_lock
+    if call_lock and ('1' in execute(f"gpio read {btn_call}"))  and hasil_call_lock > 200:
+        execute("linphonecsh generic terminate")
+        client.publish(f"call/{id}", payload="1", qos=1, retain=True)
+        calling.set()
+        print("LOG| btn call clicked")
+        before_call_lock = millis()
+        call_lock = False
 
+    
+    if '0' in execute(f"gpio read {btn_cancel}"):
+        cancel_lock = True
+        before_cancel_lock = millis()
         
-        if '0' in execute(f"gpio read {btn_cancel}"):
-            cancel_lock = True
-            before_cancel_lock = millis()
-            
-        hasil_cancel_lock = millis() - before_cancel_lock
-        if cancel_lock and ('1' in execute(f"gpio read {btn_cancel}"))  and hasil_cancel_lock > 200:
-            client.publish(f"call/{id}", payload="c", qos=1, retain=True)
-            client.publish(f"stop/{id}", payload="c", qos=1, retain=True)
-            after_calling.clear()
-            calling.clear()
-            print("LOG| btn cancel clicked")
-            before_cancel_lock = millis()
-            cancel_lock = False
-            
-        res = execute("linphonecsh status hook")
+    hasil_cancel_lock = millis() - before_cancel_lock
+    if cancel_lock and ('1' in execute(f"gpio read {btn_cancel}"))  and hasil_cancel_lock > 200:
+        client.publish(f"call/{id}", payload="c", qos=1, retain=True)
+        client.publish(f"stop/{id}", payload="c", qos=1, retain=True)
+        after_calling.clear()
+        calling.clear()
+        print("LOG| btn cancel clicked")
+        before_cancel_lock = millis()
+        cancel_lock = False
         
-        if "Incoming call" in res:
-            res = res.split()
-            res = res[-1][1:-1]
-            time.sleep(1)
-            execute(f"linphonecsh generic answer")
-        elif "hook=answered" in res:
-            execute(f"amixer set Capture {mic}%")
-            oncall = True
-            calling.clear()
-        else:
-            oncall = False
+    res = execute("linphonecsh status hook")
+    
+    if "Incoming call" in res:
+        res = res.split()
+        res = res[-1][1:-1]
+        time.sleep(1)
+        execute(f"linphonecsh generic answer")
+    elif "hook=answered" in res:
+        execute(f"amixer set Capture {mic}%")
+        oncall = True
+        calling.clear()
+    else:
+        oncall = False
 
 
-        if calling.is_set() :
-            if millis() - before_calling > 2000:
-                execute('ogg123 /home/opi1/ip-call-bed/ringback.ogg')
-                before_calling = millis()
-        
-        if after_calling.is_set() :
-            if millis() - before_calling > 1000:
-                execute(f"gpio write {led_1} 0")
-                time.sleep(0.2)
-                execute(f"gpio write {led_1} 1")
-                before_calling = millis()
-        
-        if millis() - send_activation > 5000:
-            client.publish("aktif", payload=id, qos=0, retain=False)
-            send_activation = millis()
-else :
-    while True:
-        if '0' in execute(f"gpio read {btn_call}"):
-            if not oncall and not after_calling.is_set():
-                call_lock = True
-                before_call_lock = millis()
-            
-            
-        hasil_call_lock = millis() - before_call_lock
-        if call_lock and ('1' in execute(f"gpio read {btn_call}"))  and hasil_call_lock > 200:
-            client.publish(f"assist/{id}", payload="a", qos=1, retain=True)
-            calling.set()
-            print("LOG| btn call clicked")
-            before_call_lock = millis()
-            call_lock = False
-
-        
-        if '0' in execute(f"gpio read {btn_cancel}"):
-            cancel_lock = True
-            before_cancel_lock = millis()
-            
-        hasil_cancel_lock = millis() - before_cancel_lock
-        if cancel_lock and ('1' in execute(f"gpio read {btn_cancel}"))  and hasil_cancel_lock > 200:
-            client.publish(f"assist/{id}", payload="c", qos=1, retain=True)
-            calling.clear()
-            print("LOG| btn cancel clicked")
-            before_cancel_lock = millis()
-            cancel_lock = False
-            
-        
-        if calling.is_set() :
-            if millis() - before_calling > 1000:
-                execute(f"gpio write {led_1} 0")
-                time.sleep(0.2)
-                execute(f"gpio write {led_1} 1")
-                before_calling = millis()
-        
-        if millis() - send_activation > 5000:
-            client.publish("aktif", payload=id, qos=0, retain=False)
-            send_activation = millis()
-
+    if calling.is_set() :
+        if millis() - before_calling > 2000:
+            execute('ogg123 /home/nursecall/ip-call-bed/ringback.ogg')
+            before_calling = millis()
+    
+    if after_calling.is_set() :
+        if millis() - before_calling > 1000:
+            execute(f"gpio write {led_1} 0")
+            time.sleep(0.2)
+            execute(f"gpio write {led_1} 1")
+            before_calling = millis()
+    
+    if millis() - send_activation > 5000:
+        client.publish("aktif", payload=id, qos=0, retain=False)
+        send_activation = millis()
